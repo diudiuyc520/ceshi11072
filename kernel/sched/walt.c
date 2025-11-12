d163661b70938663cf0bc78cd826d206c6bf9fa8
diudiuyc520 <121587602@qq.com>
2025-11-12 20:03:17 +0800

    Update walt.c

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -2678,7 +2678,7 @@ unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 100;
  * migrated.
  */
 unsigned int __read_mostly sched_group_downmigrate = 19000000;
-unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 95;
+unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 90;
 
 static inline
 void update_best_cluster(struct related_thread_group *grp,
